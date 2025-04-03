548260b25e62f0c8efa7273cc53f86b35c08b6f9
tomokisun <github@tomokisun.com>
2025-04-04 02:05:22 +0900

    wip

--- Makefile
+++ Makefile
@@ -12,5 +12,11 @@ build:
 		-destination platform="$(PLATFORM_IOS)"
 
 format:
-	@swift build -c release --package-path ./SwiftScripts/SwiftFormatTool --product swiftformat
-	./SwiftScripts/SwiftFormatTool/.build/release/swiftformat ./
+	find . \
+		-name '*.swift' \
+		-not -path '*/.*' -print0 \
+		| xargs -0 xcrun swift-format --ignore-unparsable-files --in-place
+
+define udid_for
+$(shell xcrun simctl list --json devices available '$(1)' | jq -r '[.devices|to_entries|sort_by(.key)|reverse|.[].value|select(length > 0)|.[0]][0].udid')
+endef
--- Playground/ContentView.swift
+++ Playground/ContentView.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        List {
-            NavigationLink("Text Shadow") {
-                TextShadowView()
-            }
-        }
-        .navigationTitle("ios-playground")
-        .navigationBarTitleDisplayMode(.inline)
+  var body: some View {
+    List {
+      NavigationLink("Text Shadow") {
+        TextShadowView()
+      }
     }
+    .navigationTitle("ios-playground")
+    .navigationBarTitleDisplayMode(.inline)
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
